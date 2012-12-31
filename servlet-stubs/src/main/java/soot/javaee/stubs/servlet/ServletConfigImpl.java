9783c798bdc65684e6490d5964b32f541c87bf2b
Bernhard Berger <berber@tzi.de>
2012-12-31 10:16:46 +0100

    Added empty stubs.

---
+++ servlet-stubs/src/main/java/soot/javaee/stubs/servlet/ServletConfigImpl.java
@@ -0,0 +1,30 @@
+package soot.javaee.stubs.servlet;
+
+import java.util.Enumeration;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+
+public class ServletConfigImpl implements ServletConfig {
+
+	public String getInitParameter(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Enumeration getInitParameterNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ServletContext getServletContext() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getServletName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
